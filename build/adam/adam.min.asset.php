35416fb5b40c9d280dcb8d1e693837a7c882abce
arunshenoy99 <devarunshenoy99@gmail.com>
2026-02-18 20:09:04 +0530

    Update build

--- build/adam/adam.min.asset.php
+++ build/adam/adam.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => '50219fc123ab9114034b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element'), 'version' => '25ddbaab2ae664696bbb');
